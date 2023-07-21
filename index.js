68f243d961759dbe07b797bb9cbb182ec0168453
Chelsea Mogwo <chelseamogwo@gmail.com>
2023-07-21 16:20:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+// // const playGame = () => {
+// //     const x = prompt('Please enter rock, paper or scissors. ')
+// //     return x
+// // }
+
+// // // module.exports = {
+// // //     playGame
+// // // }
+
+// // console.log(playGame)
+
+// // function playGame(x) {
+// //     const x = prompt('Please enter rock, paper or scissors. ')
+// //     return x
+// // }
+
+// const prompt = require('prompt-sync')
+
+
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stout
+});
+const colours = require("ansi-colors")
+const async = require("async")
+
+console.log("pick rock paper or scissors")
+
+function playGame() {
+    arr = []
+    async.series([
+        (callback) => {
+            readline.question('Pick rock, paper or scissors?', answer => {
+                    // let x = "rock";
+                    
+                    const choices = ["rock", "paper", "scissors"]
+                    const randomChoice = choices[Math.floor(Math.random() * 3)]
+                    console.log(randomChoice)
+                    if (answer === randomChoice) {
+                        arr.push("Draw");
+                        console.log(colours.yellow("Draw!"));                        
+                    } else if ((answer === "paper" && randomChoice === "rock")|| (answer === "rock" && randomChoice === "scissors") || (answer === "scissors" && randomChoice === "paper")) {
+                        arr.push("Winner")
+                        console.log(colours.green("Winner!"));                        
+                    } else if ((answer === "rock" && randomChoice ==="paper") || (answer === "scissors" && randomChoice ==="rock") || (answer === "paper" && randomChoice ==="sissors")) {
+                        arr.push("loss")
+                        console.log(colours.red("Sorry, you lose."))
+                    }
+                    
+                    callback()
+                })
+        },
+        (callback) => {
+            console.log('Do you want to play again? y/n')
+            readline.question('Do you want to play again? y/n', answer => {
+                if (answer === "y") {
+                    playGame()
+                    console.log("pick rock paper or scissors")
+                } else if (answer === "n") {
+                    callback()
+                    console.log(arr)
+                } else {
+                    callback()
+                    console.log(arr)
+                }
+                
+                
+            })
+        }
+    ], () => {
+        readline.close()
+    })
+    
+}
+
+playGame()
+
+
+
+
+
+
+
